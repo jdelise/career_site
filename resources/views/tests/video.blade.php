323539e1d2ddc4e9751ff16bc1e99e6c7499586b
Joe Delise <joedelise@gmail.com>
2015-08-03 10:20:10 -0400

    Removed urls

--- resources/views/tests/video.blade.php
+++ resources/views/tests/video.blade.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<video style="width:100%;position:absolute;bottom:0;" preload="auto" autoplay="" loop="loop" poster="http://c21scheetz.dev/webvideos/webvideo.mp4">
-    <source src="http://c21scheetz.dev/webvideos/webvideo.mp4" type="video/mp4">
-    <source src="http://c21scheetz.dev/webvideos/webvideo.webm" type="video/webm">
-    <source src="http://c21scheetz.dev/webvideos/webvideo.ogg" type="video/ogg">
+<video style="width:100%;position:absolute;bottom:0;" preload="auto" autoplay="" loop="loop" poster="/webvideos/webvideo.mp4">
+    <source src="/webvideos/webvideo.mp4" type="video/mp4">
+    <source src="/webvideos/webvideo.webm" type="video/webm">
+    <source src="/webvideos/webvideo.ogg" type="video/ogg">
 </video>
 
 </body>
